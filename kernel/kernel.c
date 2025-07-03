4c76273af99eb5eab8957f5b663246fb6b4b2ba6
OuranOS-Project <ouranos.dev0@gmail.com>
2025-07-03 14:59:41 +0200

    Os doesn't boot

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,9 @@
-void main() {
-    while (1) {}
+void _start() {
+    volatile char *video = (volatile char*)0xB8000;
+    video[0] = 'O'; video[1] = 0x07;  // Affiche 'O'
+    video[2] = 'K'; video[3] = 0x07;  // Affiche 'K'
+
+    while (1) {
+        // Boucle infinie pour éviter de quitter le programme
+    }
 }
